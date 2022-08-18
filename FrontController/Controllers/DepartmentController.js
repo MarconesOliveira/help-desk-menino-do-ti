7d4bfca991eb6828ddfc4766c5e7c42575702365
Marcones Oliveira <marcones.oliveira.ifrn@gmail.com>
2022-08-18 03:49:02 -0300

    Routes from Department

--- FrontController/Controllers/DepartmentController.js
+++ FrontController/Controllers/DepartmentController.js
@@ -1,23 +1,37 @@
 import Department from "../../Models/Department.js";
 
 export async function getAllDepartments(req, res) {
-    const departments = Department.find();
-    console.log(departments);
+    const departments = await Department.find();
     res.send(departments);
 }
 
 export async function addDepartment(req, res) {
-    
+    const department = new Department(req.body);
+    department.save()
+        .then(() => (res.send("Department Saved on Database.")))
+        .catch((error) => (console.log(error)));
 }
 
 export async function getDepartment(req, res) {
-    
+    const department = await Department.findOne({
+        $or:[{name: req.params.id},{_ID: req.params.id}]
+    });
+    res.send(department);
 }
 
 export async function updateDepartment(req, res) {
-    
+    const update = req.body;
+    const result = await Department.updateOne({
+        $or:[{name: req.params.id},{_ID: req.params.id}]
+    },{
+        ...update
+    });
+    res.send(result);
 }
 
 export async function deleteDepartment(req, res) {
-    
+    const result = await Department.deleteOne({
+        $or:[{name: req.params.id},{_ID: req.params.id}]
+    });
+    res.send(result);
 }
