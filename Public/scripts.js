c94ef04b408f0bc00e9628d07a9aa98a0a8ab05b
Marcones Oliveira <marcones.oliveira.ifrn@gmail.com>
2022-08-24 00:19:00 -0300

    Update scripts

--- Public/scripts.js
+++ Public/scripts.js
@@ -1,4 +1,13 @@
 
+const parseJwt = (token) => {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
 const saveToken = (token) => {
     localStorage.setItem("token_menino_do_ti", token);
 }
@@ -7,4 +16,5 @@ const getToken = () => {
 }
 const deleteToken = () => {
     localStorage.removeItem("token_menino_do_ti");
+    window.location.href = "/";
 }
