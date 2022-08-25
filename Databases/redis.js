7fe22fc57d066b6645601e1dc076817c0f22fa68
Marcones Oliveira <marcones.oliveira.ifrn@gmail.com>
2022-08-25 14:51:11 -0300

    Feature redis

---
+++ Databases/redis.js
@@ -0,0 +1,12 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+await client.connect();
+
+await client.set('key', 'value');
+const value = await client.get('key');
+
+export default client;
